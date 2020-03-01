b355bea81ebdb3a7a5025acef50e34e5d1e0f303
aferryc <aferryc@gmail.com>
2020-03-01 08:17:46 +0700

    add url length

--- products/models.py
+++ products/models.py
@@ -4,10 +4,10 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
-    url = models.URLField(max_length=200)
+    url = models.URLField(max_length=300)
     price = models.CharField(max_length=100)
     image_list = models.TextField()
-    main_image = models.CharField(max_length=100)
+    main_image = models.CharField(max_length=300)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
